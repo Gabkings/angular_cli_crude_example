cd76e97ca327447d187653c73ed1756b6ea566d6
Gabriel Gitonga <gabriel.gitonga@bskglobaltech.com>
2021-03-17 14:18:37 +0300

    Generating products using facker js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http'
+import { MatToolbarModule} from '@angular/material/toolbar'
+import { MatIconModule} from '@angular/material/icon'
+import { MatCardModule} from '@angular/material/card'
+import { MatButtonModule} from '@angular/material/button'
+import { MatProgressSpinnerModule} from '@angular/material/progress-spinner'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
